c31eda5d2be8643c46aed7aa9f42b2dfc9b46d4a
Raphael Pizzaia <raphapizzaia@gmail.com>
2019-12-22 17:49:54 -0300

    Initial Structure

---
+++ MagoTrader.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MagoTrader.Services
+{
+    public class Class1
+    {
+    }
+}
---
+++ MagoTrader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MagoTrader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
