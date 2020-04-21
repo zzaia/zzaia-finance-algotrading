b329a1924ba1c5c5361289470e2332f9fb04c12a
Raphael Pizzaia <raphael.pizzaia@transferoswiss.ch>
2020-04-21 19:36:57 -0300

    Improve OHLCV model

--- MagoTrader.Core/Models/OHLCV.cs
+++ MagoTrader.Core/Models/OHLCV.cs
@@ -4,16 +4,89 @@ using System.Collections.Generic;
 
 namespace MagoTrader.Core.Models
 {
+    /// <summary>
+    /// Represents a financial market candlestick.
+    /// </summary>
     public class OHLCV
     {
+        /// <summary>
+        /// Represents exchange where the data was taken.
+        /// </summary>
         public ExchangeNameEnum Exchange { get; set; }
-        public AssetTickerEnum Ticker { get; set; }
+
+        /// <summary>
+        /// Represents the data market, with main and base assets.
+        /// </summary>
+        public Market Market { get; set; }
+
+        /// <summary>
+        /// Represents the candlestick time of measurement.
+        /// </summary>
         public DateTime DateTime { get; set; }
-        public Decimal Open { get; set; }
-        public Decimal High { get; set; }
-        public Decimal Low { get; set; }
-        public Decimal Close { get; set; }
-        public Decimal Volume { get; set; }
+
+        /// <summary>
+        /// Represents the candlestick timeframe.
+        /// </summary>
+        public TimeFrame TimeFrame { get; set; }
+        
+        /// <summary>
+        /// Opening main asset price for timeframe candlestick.
+        /// </summary>
+        public decimal Open { get; set; }
+        
+        /// <summary>
+        /// Highest main asset price for timeframe candlestick.
+        /// </summary>
+        public decimal High { get; set; }
+
+        /// <summary>
+        /// Lowest main asset price for timeframe candlestick.
+        /// </summary>
+        public decimal Low { get; set; }
+
+        /// <summary>
+        /// Closing main asset price for timeframe candlestick.
+        /// </summary>
+        public decimal Close { get; set; }
+
+        /// <summary>
+        /// Last main asset price, without closing the timeframe candlestick.
+        /// </summary>
+        public decimal Last { get; set; }
+
+        /// <summary>
+        /// Biggest asset price for selling in the timeframe candlestick.
+        /// </summary>
+        public decimal Sell { get; set; }
+
+        /// <summary>
+        /// Biggest asset price for buying in the timeframe candlestick.
+        /// </summary>
+        public decimal Buy { get; set; }
+
+        /// <summary>
+        /// Volume of value, in market base asset, traded in the timeframe.
+        /// </summary>
+        public decimal Volume { get; set; }
+
+        /// <summary>
+        /// Total quantity of main asset traded in the timeframe.
+        /// </summary>
+        public decimal TradedQuantity { get; set; }
+
+        /// <summary>
+        /// Average price of main asset traded in the timeframe.
+        /// </summary>
+        public decimal Average { get; set; }
+
+        /// <summary>
+        /// Total number of trades in the timeframe.
+        /// </summary>
+        public int NumberOfTrades { get; set; }
+
+        /// <summary>
+        /// Order book for market main asset, from OHLCV date time.
+        /// </summary>
         public OrderBook OrderBook { get; set; }
 
     }
