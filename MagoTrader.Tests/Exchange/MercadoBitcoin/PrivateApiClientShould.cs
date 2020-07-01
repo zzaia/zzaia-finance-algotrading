83ee022b1dccc4734077fab7ecce1725232e280b
Raphael Pizzaia <raphael.pizzaia@transferoswiss.ch>
2020-07-01 18:42:31 -0300

    Add private api client test

---
+++ MagoTrader.Tests/Exchange/MercadoBitcoin/PrivateApiClientShould.cs
@@ -0,0 +1,16 @@
+﻿using MagoTrader.Exchange.MercadoBitcoin.Private;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagoTrader.Tests.Exchange.MercadoBitcoin
+{
+    public class PrivateApiClientShould
+    {
+        private readonly PrivateApiClient _client;
+        public PrivateApiClientShould()
+        {
+
+        }
+    }
+}
