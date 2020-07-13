ea6f4335ac1dda5cbcbbab8a84154e754890e6d8
Raphael Pizzaia <raphael.pizzaia@transferoswiss.ch>
2020-07-13 14:46:36 -0300

    Implement system message end-point testing

--- MagoTrader.Tests/Exchange/MercadoBitcoin/PrivateApiClientShould.cs
+++ MagoTrader.Tests/Exchange/MercadoBitcoin/PrivateApiClientShould.cs
@@ -1,16 +1,45 @@
-﻿using MagoTrader.Exchange.MercadoBitcoin.Private;
+﻿using MagoTrader.Core.Exchange;
+using MagoTrader.Exchange.MercadoBitcoin.Models;
+using MagoTrader.Exchange.MercadoBitcoin.Private;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Net.Http;
+using Xunit;
 
 namespace MagoTrader.Tests.Exchange.MercadoBitcoin
 {
     public class PrivateApiClientShould
     {
         private readonly PrivateApiClient _client;
+        private readonly IConfiguration _configuration;
         public PrivateApiClientShould()
         {
-
+            var htttpClient = new HttpClient();
+            var logger = new Logger<PrivateApiClient>(new LoggerFactory());
+            _client = new PrivateApiClient(htttpClient, logger);
+            _client.SetBaseAddress(new Uri("https://www.mercadobitcoin.net"));
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("secrets.json")
+                .AddUserSecrets<MagoTrader.ServerApp.Startup>()
+                .Build();
         }
+
+        [Fact]
+        public async void PostSytemsMessages()
+        {
+            //Arrange:
+            ClientCredential clientCredential = new ClientCredential();
+            _configuration.Bind("Exchange:MercadoBitcoin:Private", clientCredential);
+
+            //Act:
+            var response = await _client.PostSystemMessagesAsync(clientCredential, SystemMessageTypeEnum.ERROR);
+
+            //Assert:
+            Assert.True(response.Success);
+            Assert.NotNull(response);
+            Assert.True(response.Output.IsSuccess());
+        }
+
     }
 }
