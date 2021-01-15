2d84c04bdf7a31b08613b07de1bd198269712bb1
Raphael Pizzaia <raphael.pizzaia@transferoswiss.ch>
2021-01-14 22:04:26 -0300

    Implement the connect to rest method in connector service

--- MarketIntelligency.DataEventManager/ConnectorService.cs
+++ MarketIntelligency.DataEventManager/ConnectorService.cs
@@ -1,13 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using MarketIntelligency.Core.Interfaces.ExchangeAggregate;
+using MarketIntelligency.Core.Models;
+using MarketIntelligency.Core.Models.EnumerationAggregate;
+using MediatR;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MarketIntelligency.DataEventManager
 {
     public class ConnectorService
     {
+        private readonly IExchange _exchange;
+        private readonly IMediator _mediator;
 
+        public ConnectorService(IExchange exchange, IMediator mediator)
+        {
+            _exchange = exchange ?? throw new ArgumentNullException(nameof(exchange));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        private async Task ConnectToRest<T, TResult>(T parameter, CancellationToken cancelationToken, TimeFrame timeFrame, Func<T, ObjectResult<TResult>> method) where TResult : class
+        {
+            while (!cancelationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var response = method.Invoke(parameter);
+                    if (response.Succeed)
+                    {
+                        var eventToPublish = new EventSource<TResult>(response.Output);
+                        await _mediator.Publish(eventToPublish);
+                    }
+                    else
+                    {
+
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
     }
 }
