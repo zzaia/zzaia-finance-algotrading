cb8b6de61f8b46305e2b4b8634982d97b7e2cc54
Raphael Pizzaia <raphael.pizzaia@transferoswiss.ch>
2021-01-15 07:55:50 -0300

    Add logging pattern class to connector service

--- MarketIntelligency.DataEventManager/ConnectorService.cs
+++ MarketIntelligency.DataEventManager/ConnectorService.cs
@@ -2,28 +2,37 @@
 using MarketIntelligency.Core.Models;
 using MarketIntelligency.Core.Models.EnumerationAggregate;
 using MediatR;
+using Microsoft.ApplicationInsights;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace MarketIntelligency.DataEventManager
 {
-    public class ConnectorService
+    public partial class ConnectorService
     {
         private readonly IExchange _exchange;
         private readonly IMediator _mediator;
+        protected ILogger<ConnectorService> _logger;
+        private readonly TelemetryClient _telemetryClient;
 
-        public ConnectorService(IExchange exchange, IMediator mediator)
+        public ConnectorService(IExchange exchange, IMediator mediator, ILogger<ConnectorService> logger, TelemetryClient telemetryClient)
         {
             _exchange = exchange ?? throw new ArgumentNullException(nameof(exchange));
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _telemetryClient = telemetryClient ?? throw new ArgumentNullException(nameof(telemetryClient));
         }
 
         private async Task ConnectToRest<T, TResult>(T parameter, CancellationToken cancellationToken, TimeFrame timeFrame, Func<T, CancellationToken, ObjectResult<TResult>> method) where TResult : class
         {
-            while (!cancellationToken.IsCancellationRequested)
+            Log.ConnectToRest.Received(_logger);
+            Log.ConnectToRest.ReceivedAction(_telemetryClient);
+            try
             {
-                try
+                while (!cancellationToken.IsCancellationRequested)
                 {
                     var response = method.Invoke(parameter, cancellationToken);
                     if (response.Succeed)
@@ -33,12 +42,14 @@ namespace MarketIntelligency.DataEventManager
                     }
                     else
                     {
-
+                        var errorPayload = JsonSerializer.Serialize(response.Error);
+                        Log.ConnectToRest.WithFailedResponse(_logger, errorPayload);
                     }
                 }
-                catch (Exception ex)
-                {
-                }
+            }
+            catch (Exception ex)
+            {
+                Log.ConnectToRest.WithException(_logger, ex);
             }
         }
     }
