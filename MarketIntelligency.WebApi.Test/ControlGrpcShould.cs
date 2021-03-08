ffbe74163d88fc26d5f25d7e4e956106fd8d730c
Raphael Pizzaia <raphael.pizzaia@transferoswiss.ch>
2021-03-07 22:02:37 -0300

    Implement a example test for grpc services

--- MarketIntelligency.WebApi.Test/ControlGrpcShould.cs
+++ MarketIntelligency.WebApi.Test/ControlGrpcShould.cs
@@ -1,3 +1,10 @@
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Core.Testing;
+using MarketIntelligency.WebApi.Grpc.Protos;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace MarketIntelligency.Api.Test
@@ -7,7 +14,18 @@ namespace MarketIntelligency.Api.Test
         [Fact]
         public void ActivateAGivenExchange()
         {
+            // Arrange
+            var mockClient = new Mock<ControlGrpc.ControlGrpcClient>();
+            var controlArgs = new ControlMetadata() { Name = "connectors" };
+            // Use a factory method provided by Grpc.Core.Testing.TestCalls to create an instance of a call.
+            var fakeCall = TestCalls.AsyncUnaryCall(Task.FromResult(new Empty()), Task.FromResult(new Metadata()), () => Status.DefaultSuccess, () => new Metadata(), () => { });
 
+            // Act
+            mockClient.Setup(m => m.ActivateAsync(It.IsAny<ControlMetadata>()
+                , null, null, CancellationToken.None)).Returns(fakeCall);
+            
+            // Assert
+            Assert.Equal(fakeCall, mockClient.Object.ActivateAsync(controlArgs));
         }
 
         [Fact]
