a19f48d9e41a3059d0378988e9723777176a4746
Raphael Pizzaia <raphael.pizzaia@transferoswiss.ch>
2022-02-13 20:59:59 -0300

    Remove dapr related files

--- dapr.launch.ps1
+++
@@ -1,8 +0,0 @@
-# Adapters applications:
-dapr run --app-id "binance-adapter" --app-protocol "grpc" --app-port "5001" --dapr-grpc-port "50010" -- dotnet run --project ./src/Application/Adapter/Binance/src/MarketIntelligency.Application.Adapter.Binance.csproj --urls="http://+:5001"
-
-# Event Managers:
-dapr run --app-id "data-event-manager" --app-port "5002" --dapr-grpc-port "50011" --dapr-http-port "5011" -- dotnet run --project ./src/Application/DataEventManager/src/MarketIntelligency.Application.DataEventManager.csproj --urls="http://+:5002"
-
-# Strategies:
-dapr run --app-id "strategies-services" --app-port "5003" --dapr-grpc-port "50012" --dapr-http-port "5012" -- dotnet run --project ./src/Application/Strategies/src/MarketIntelligency.Application.Strategies.csproj --urls="http://+:5003"
