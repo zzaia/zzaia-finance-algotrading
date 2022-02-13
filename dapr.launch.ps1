57d780700b609abb0445fa9c44cb27a4e82c0dfb
Raphael Pizzaia <raphael.pizzaia@transferoswiss.ch>
2022-02-13 20:56:50 -0300

    add launch configurations

--- dapr.launch.ps1
+++ dapr.launch.ps1
@@ -1,5 +1,5 @@
 # Adapters applications:
-dapr run --app-id "binance-adapter" --app-port "5001" --dapr-grpc-port "50010" --dapr-http-port "5010" -- dotnet run --project ./src/Application/Adapter/Binance/src/MarketIntelligency.Application.Adapter.Binance.csproj --urls="http://+:5001"
+dapr run --app-id "binance-adapter" --app-protocol "grpc" --app-port "5001" --dapr-grpc-port "50010" -- dotnet run --project ./src/Application/Adapter/Binance/src/MarketIntelligency.Application.Adapter.Binance.csproj --urls="http://+:5001"
 
 # Event Managers:
 dapr run --app-id "data-event-manager" --app-port "5002" --dapr-grpc-port "50011" --dapr-http-port "5011" -- dotnet run --project ./src/Application/DataEventManager/src/MarketIntelligency.Application.DataEventManager.csproj --urls="http://+:5002"
