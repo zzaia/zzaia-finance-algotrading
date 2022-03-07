83370ed868b180a4c64882d50b7baf345f47d0dc
Raphael Pizzaia <raphael.pizzaia@transferoswiss.ch>
2022-03-07 09:21:43 -0300

    Improve the date time offset mapping

--- src/Web/Grpc/src/Models/MappingProfile.cs
+++ src/Web/Grpc/src/Models/MappingProfile.cs
@@ -5,7 +5,6 @@ using MarketIntelligency.Core.Models.EnumerationAggregate;
 using MarketIntelligency.Core.Models.MarketAgregate;
 using MarketIntelligency.Core.Models.OrderBookAggregate;
 using MarketIntelligency.Web.Grpc.Protos;
-using System;
 
 namespace MarketIntelligency.Web.Grpc.Models
 {
@@ -16,11 +15,11 @@ namespace MarketIntelligency.Web.Grpc.Models
             CreateMap<OrderBook, OrderBookDTO>()
                 .ForMember(dst => dst.ExchangeName, opt => opt.MapFrom(src => src.Exchange.DisplayName))
                 .ForMember(dst => dst.Market, opt => opt.MapFrom(src => src.Market.Ticker))
-                .ForMember(dst => dst.ServerTimeStamp, opt => opt.MapFrom(src => src.ServerTimeStamp.ToString()));
+                .ForMember(dst => dst.ServerTimeStamp, opt => opt.MapFrom(src => Timestamp.FromDateTimeOffset(src.ServerTimeStamp)));
             CreateMap<OrderBookDTO, OrderBook>()
                 .ForMember(dst => dst.Exchange, opt => opt.MapFrom(src => Enumeration.FromDisplayName<ExchangeName>(src.ExchangeName)))
                 .ForMember(dst => dst.Market, opt => opt.MapFrom(src => new Market(src.Market)))
-                .ForMember(dst => dst.ServerTimeStamp, opt => opt.MapFrom(src => DateTimeOffset.Parse(src.ServerTimeStamp)));
+                .ForMember(dst => dst.ServerTimeStamp, opt => opt.MapFrom(src => src.ServerTimeStamp.ToDateTimeOffset()));
             CreateMap<OrderBookLevelDTO, OrderBookLevel>()
                 .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dst => dst.Price, opt => opt.MapFrom(src => (decimal)src.Price))
@@ -31,12 +30,12 @@ namespace MarketIntelligency.Web.Grpc.Models
                 .ForMember(dst => dst.Amount, opt => opt.MapFrom(src => (DecimalValue)src.Amount));
             CreateMap<EventSourceDTO, EventSource<OrderBook>>()
                 .ForMember(dst => dst.Content, opt => opt.MapFrom((a, b, c, d) => d.Mapper.Map<OrderBook>(a.Content.Unpack<OrderBookDTO>())))
-                .ForMember(dst => dst.RecordedAt, opt => opt.MapFrom(src => DateTimeOffset.Parse(src.RecordedAt)))
-                .ForMember(dst => dst.OcurredAt, opt => opt.MapFrom(src => DateTimeOffset.Parse(src.OcurredAt)));
+                .ForMember(dst => dst.RecordedAt, opt => opt.MapFrom(src => src.RecordedAt.ToDateTimeOffset()))
+                .ForMember(dst => dst.OcurredAt, opt => opt.MapFrom(src => src.OcurredAt.ToDateTimeOffset()));
             CreateMap<EventSource<OrderBook>, EventSourceDTO>()
                 .ForMember(dst => dst.Content, opt => opt.MapFrom((a, b, c, d) => Any.Pack(d.Mapper.Map<OrderBookDTO>(a.Content))))
-                .ForMember(dst => dst.RecordedAt, opt => opt.MapFrom(src => src.RecordedAt.ToString()))
-                .ForMember(dst => dst.OcurredAt, opt => opt.MapFrom(src => src.OcurredAt.ToString()));
+                .ForMember(dst => dst.RecordedAt, opt => opt.MapFrom(src => Timestamp.FromDateTimeOffset(src.RecordedAt)))
+                .ForMember(dst => dst.OcurredAt, opt => opt.MapFrom(src => Timestamp.FromDateTimeOffset(src.OcurredAt)));
         }
     }
 }
